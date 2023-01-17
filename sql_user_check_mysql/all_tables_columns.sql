70fc781f2e925f6b466719d78bc9d9f6ad2b5a7d
ravikant-lb <ravikant@lightbeam.ai>
2023-01-17 17:35:37 +0530

    Add mysql test queries.

---
+++ sql_user_check_mysql/all_tables_columns.sql
@@ -0,0 +1,36 @@
+SELECT DISTINCT 'default'                       AS schema_name,
+                c.table_name                    AS table_name,
+                COALESCE(tt.TABLE_COMMENT, '')  AS table_description,
+                c.column_name                   AS column_name,
+                c.data_type                     AS data_type,
+                COALESCE(c.column_comment, ' ') AS column_description,
+                ordinal_position                AS column_id,
+                c.table_schema                  AS `database`,
+                FALSE                           AS is_view,
+                COALESCE(tt.table_rows, 0)      AS num_rows,
+                CASE
+                    WHEN lower(c.is_nullable) = 'yes' THEN
+                        TRUE
+                    WHEN lower(c.is_nullable) = 'no' THEN
+                        FALSE
+                    END                         AS is_nullable,
+                cc.constraint_type,
+                COALESCE(tt.data_length, 0)     AS table_size
+FROM (SELECT *
+      FROM INFORMATION_SCHEMA.COLUMNS) AS c
+         LEFT JOIN INFORMATION_SCHEMA.tables AS tt ON c.table_schema = tt.table_schema
+    AND c.table_name = tt.table_name
+         LEFT JOIN (SELECT DISTINCT tc.table_schema AS SCHEMA_NAME,
+                                    kcu.table_name,
+                                    kcu.column_name,
+                                    max(tc.constraint_type)
+                                                    AS constraint_type
+                    FROM information_schema.table_constraints AS tc
+                             JOIN information_schema.key_column_usage kcu ON tc.constraint_name = kcu.constraint_name
+                    WHERE tc.constraint_type in ('PRIMARY KEY', 'FOREIGN KEY')
+                    GROUP BY tc.table_schema,
+                             kcu.table_name,
+                             kcu.column_name) AS cc ON c.column_name = cc.column_name
+    AND c.table_schema = cc.schema_name
+    AND c.table_name = cc.table_name
+ORDER BY c.table_schema, c.table_name;
---
+++ sql_user_check_mysql/fetch_all_relations.sql
@@ -0,0 +1,28 @@
+SELECT l_schema_name AS l_database_name,
+       'default'     AS l_schema_name,
+       r_schema_name AS r_database_name,
+       'default'     AS r_schema_name,
+       constraint_name,
+       l_table_name,
+       r_table_name,
+       l_column_name,
+       r_column_name,
+       pk_constraint_name,
+       fk_constraint_name
+FROM (SELECT DISTINCT concat_ws('_', pk_constraint_name, fk_constraint_name) AS constraint_name,
+                      rc1.*,
+                      kcu1.table_schema                                      AS r_schema_name,
+                      kcu1.table_name                                        AS r_table_name,
+                      kcu1.column_name                                       AS r_column_name
+      FROM information_schema.KEY_COLUMN_USAGE AS kcu1
+               JOIN (SELECT table_schema AS l_schema_name,
+                            table_name   AS l_table_name,
+                            column_name  AS l_column_name,
+                            pk_constraint_name,
+                            fk_constraint_name
+                     FROM information_schema.key_column_usage AS kcu
+                              JOIN (SELECT constraint_name        AS fk_constraint_name,
+                                           unique_constraint_name AS pk_constraint_name
+                                    FROM information_schema.referential_constraints) AS rc
+                                   ON kcu.constraint_name = pk_constraint_name) AS rc1
+                    ON kcu1.constraint_name = rc1.fk_constraint_name) TBL;
