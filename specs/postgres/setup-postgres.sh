03ce5d823b38df286a88cc99810a0ac2150da939
Arpit Solanki <arpit@lightbeam.ai>
2023-02-05 18:57:24 +0530

    Option to import and destroy with configurable names

--- specs/postgres/setup-postgres.sh
+++ specs/postgres/setup-postgres.sh
@@ -3,12 +3,14 @@
 set -euxo
 
 readonly WORK_DIR="$(mktemp -d)"
-STORAGE_CLASS_NAME=$1
-STORAGE_SIZE=$2
-NAMESPACE=$3
-DATABASE_DUMP_FILE_PATH=$4
-DATABASE_NAME=$5
-POSTGRES_PASSWORD=$6
+ACTION=$1
+STORAGE_CLASS_NAME=$2
+STORAGE_SIZE=$3
+NAMESPACE=$4
+DATABASE_DUMP_FILE_PATH=$5
+DATABASE_NAME=$6
+POSTGRES_PASSWORD=$7
+NAME=$8
 SQL_CREATE_DB_STMT="CREATE DATABASE $DATABASE_NAME;"
 
 mkdir -p overlays
@@ -41,7 +43,7 @@ cat <<'EOF' > overlays/kustomization.yaml
 apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 commonLabels:
-  app: lb-postgres
+  app: RELEASE_NAME
 namespace: NAMESPACE
 resources:
   - ../base
@@ -49,27 +51,66 @@ resources:
 patchesStrategicMerge:
   - pv-patch.yaml
   - pg_secret.yaml
+patches:
+- target:
+    kind: Secret
+    name: .*
+  patch: |-
+    - op: replace
+      path: /metadata/name
+      value: RELEASE_NAME
+- target:
+    kind: Service
+    name: .*
+  patch: |-
+    - op: replace
+      path: /metadata/name
+      value: RELEASE_NAME
+- target:
+    kind: PersistentVolumeClaim
+    name: .*
+  patch: |-
+    - op: replace
+      path: /metadata/name
+      value: RELEASE_NAME
+- target:
+    kind: Deployment
+    name: .*
+  patch: |-
+    - op: replace
+      path: /metadata/name
+      value: RELEASE_NAME
 EOF
 
 sed -i "s/STORAGE_CLASS_NAME/$STORAGE_CLASS_NAME/g" overlays/pv-patch.yaml
 sed -i "s/STORAGE_SIZE/$STORAGE_SIZE/g" overlays/pv-patch.yaml
 sed -i "s/NAMESPACE/$NAMESPACE/g" overlays/kustomization.yaml
+sed -i "s/RELEASE_NAME/$NAME/g" overlays/kustomization.yaml
 sed -i "s/NAMESPACE/$NAMESPACE/g" overlays/pg_secret.yaml
 pgPassword=$(echo "$POSTGRES_PASSWORD" | base64)
 sed -i "s/PG_PASSWORD/$pgPassword/g" overlays/pg_secret.yaml
 kubectl create ns "$NAMESPACE" || true
-kubectl kustomize overlays/ | kubectl apply -f -
+kubectl kustomize overlays/
+kubectl kustomize overlays/ | kubectl "$ACTION" -f -
 rm -rf overlays
-kubectl wait --for=condition=ready pod -l app=lb-postgres  -n "$NAMESPACE" --timeout 300s
+if [[ "$ACTION" == delete ]]; then
+  exit 0
+fi
+kubectl wait --for=condition=ready pod -l app="$NAME"  -n "$NAMESPACE" --timeout 300s
 pushd "$WORK_DIR"
 if [[ "$DATABASE_DUMP_FILE_PATH" == s3://* ]]; then
   aws s3 cp "$DATABASE_DUMP_FILE_PATH" dump.sql.gz
 else
   cp "$DATABASE_DUMP_FILE_PATH" dump.sql.gz
 fi
-gzip -d dump.sql.gz
-pgPod=$(kubectl get pods -l app=lb-postgres -o 'jsonpath={.items[0].metadata.name}')
+if [[ "$DATABASE_DUMP_FILE_PATH" == *.gz ]]; then
+  gzip -d dump.sql.gz
+else
+  mv dump.sql.gz dump.sql
+fi
+
+pgPod=$(kubectl get pods -l app="$NAME" -o 'jsonpath={.items[0].metadata.name}')
 kubectl cp "$(ls *.sql)" "$pgPod":/tmp/
-filesList=$(kubectl exec deploy/lb-postgres -- ls /tmp/)
-kubectl exec deploy/lb-postgres -- psql --username postgres -c "$SQL_CREATE_DB_STMT"
-kubectl exec deploy/lb-postgres -- psql --username postgres -d "$DATABASE_NAME" -f /tmp/"$filesList"
+filesList=$(kubectl exec deploy/"$NAME" -- ls /tmp/)
+kubectl exec deploy/"$NAME" -- psql --username postgres -c "$SQL_CREATE_DB_STMT"
+kubectl exec deploy/"$NAME" -- psql --username postgres -d "$DATABASE_NAME" -f /tmp/"$filesList"
