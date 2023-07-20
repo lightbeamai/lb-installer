b2e4aa7d3ce009dd7536064b835ca32bcfad9043
kaif-lb <136313242+kaif-lb@users.noreply.github.com>
2023-07-20 14:55:43 +0530

    LA-0000 Add script to fetch database stats for postgres (#36)
    * LA-0000 Add script to fetch database stats for postgres
    
    - script to fetch some aggregate values
    
    * support two modes in script
    
    * address review comments
    
    * check if psql is installed

---
+++ sql_user_check_postgres/database_stats.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+port="5432"
+mode="stats"
+
+while getopts h:u:d:m:o:p flag
+do
+    case "${flag}" in
+        h) dbhost=${OPTARG};;
+        u) username=${OPTARG};;
+        d) database=${OPTARG};;
+        m) mode=${OPTARG};;
+        o) outputfile=${OPTARG};;
+        p) port=${OPTARG};;
+    esac
+done
+
+
+echo "dbhost: $dbhost";
+echo "username: $username";
+echo "database: $database";
+echo "port: $port";
+echo "mode: $mode";
+echo "outputfile: $outputfile";
+
+# Check if psql command is available
+if command -v psql &>/dev/null; then
+    echo "psql is installed and available."
+else
+    echo "psql is NOT installed or not in the system's PATH."
+fi
+
+if [ -z "$dbhost" ] || [ -z "$username" ] || [ -z "$database" ] || [ -z "$outputfile" ]; then
+        echo 'Missing mandatory args: -h <DB_HOST>, -u <DB_USER>, -o <OUTPUT_FILE> or -d <DATABASE_NAME>' >&2
+        exit 1
+fi
+
+
+if [ "$mode" == "stats" ]; then
+  echo -e "\n\n===============Database list with size============" > ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./database_list_with_size.sql -p $port -d $database >> $outputfile
+
+  echo -e "\n\n===============Total tables, rows, columns and size per schema============" >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./tables_rows_columns_size_per_schema.sql -p $port -d $database >> $outputfile
+
+  echo -e "\n\n===============Data type distribution============" >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./data_type_distribution.sql -p $port -d $database >> $outputfile
+
+elif [ "$mode" == "full_metadata" ]; then
+  echo -e "\n\n===============Complete metadata============" > ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./complete_metadata.sql -p $port -d $database >> $outputfile
+
+  echo -e "\n\n===============Referential Relations============" >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./referential_relations.sql -p $port -d $database >> $outputfile
+
+else
+  echo "Mode should be either stats or full_metadata, found: $mode"
+  exit 1
+fi
+
+
