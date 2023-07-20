b5bbdb3c1a82b52d5293fb3e5238b89f8e9240ed
Kaif <kaif@lightbeam.ai>
2023-07-20 14:13:36 +0530

    address review comments

--- sql_user_check_postgres/README.md
+++ sql_user_check_postgres/README.md
@@ -1,21 +1,20 @@
+# Postgres User Check
+
 database_stats.sh script can be used to test connectivity with Postgres database as well as fetch some stats and metadata. 
 The script supports 2 modes which can be specified with -m flag while running the script. One mode is stats mode which 
 will just print info like list of databases with their size. Count of tables, rows, column, size per schema. 
 Distribution of data by data type. Stats mode is the default mode. Another mode is full_metadata which will print 
 complete information of all columns like their data type, constraints etc.
 
-From sql_user_check_postgres directory 
+From `sql_user_check_postgres` directory, run these commands depending on mode of script. This will ask
+for database password.
 
 stats Mode:
-```sql
-./database_stats.sh -u <DATABASE_USER> -h <DATABASE_HOSTNAME> -d <DATABASE_NAME> 
+```shell
+./database_stats.sh -u <DATABASE_USER> -h <DATABASE_HOSTNAME> -d <DATABASE_NAME> -o <OUTPUT_FILE>
 ```
 
 full_metadata Mode:
-```sql
-./database_stats.sh -u <DATABASE_USER> -h <DATABASE_HOSTNAME> -d <DATABASE_NAME> -m full_metadata
+```shell
+./database_stats.sh -u <DATABASE_USER> -h <DATABASE_HOSTNAME> -d <DATABASE_NAME> -m full_metadata -o <OUTPUT_FILE>
 ```
-The script will output details in this file ~/test_database_stats.out
-
-
-
--- sql_user_check_postgres/database_stats.sh
+++ sql_user_check_postgres/database_stats.sh
@@ -3,13 +3,14 @@
 port="5432"
 mode="stats"
 
-while getopts h:u:d:m:p flag
+while getopts h:u:d:m:o:p flag
 do
     case "${flag}" in
         h) dbhost=${OPTARG};;
         u) username=${OPTARG};;
         d) database=${OPTARG};;
         m) mode=${OPTARG};;
+        o) outputfile=${OPTARG};;
         p) port=${OPTARG};;
     esac
 done
@@ -20,32 +21,34 @@ echo "username: $username";
 echo "database: $database";
 echo "port: $port";
 echo "mode: $mode";
+echo "outputfile: $outputfile";
 
-if [ -z "$dbhost" ] || [ -z "$username" ] || [ -z "$database" ]; then
-        echo 'Missing mandatory args: -h <DB_HOST>, -u <DB_USER> or -d <DATABASE_NAME>' >&2
+
+if [ -z "$dbhost" ] || [ -z "$username" ] || [ -z "$database" ] || [ -z "$outputfile" ]; then
+        echo 'Missing mandatory args: -h <DB_HOST>, -u <DB_USER>, -o <OUTPUT_FILE> or -d <DATABASE_NAME>' >&2
         exit 1
 fi
 
 
 if [ "$mode" == "stats" ]; then
   echo -e "\n\n===============Database list with size============" > ~/test_database_stats.out
-  psql -h $dbhost -U $username -f ./database_list_with_size.sql -p $port -d $database >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./database_list_with_size.sql -p $port -d $database >> $outputfile
 
   echo -e "\n\n===============Total tables, rows, columns and size per schema============" >> ~/test_database_stats.out
-  psql -h $dbhost -U $username -f ./tables_rows_columns_size_per_schema.sql -p $port -d $database >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./tables_rows_columns_size_per_schema.sql -p $port -d $database >> $outputfile
 
   echo -e "\n\n===============Data type distribution============" >> ~/test_database_stats.out
-  psql -h $dbhost -U $username -f ./data_type_distribution.sql -p $port -d $database >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./data_type_distribution.sql -p $port -d $database >> $outputfile
 
 elif [ "$mode" == "full_metadata" ]; then
   echo -e "\n\n===============Complete metadata============" > ~/test_database_stats.out
-  psql -h $dbhost -U $username -f ./complete_metadata.sql -p $port -d $database >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./complete_metadata.sql -p $port -d $database >> $outputfile
 
   echo -e "\n\n===============Referential Relations============" >> ~/test_database_stats.out
-  psql -h $dbhost -U $username -f ./referential_relations.sql -p $port -d $database >> ~/test_database_stats.out
+  psql -h $dbhost -U $username -f ./referential_relations.sql -p $port -d $database >> $outputfile
 
 else
-  echo "Mode should be either stats or full_metadata"
+  echo "Mode should be either stats or full_metadata, found: $mode"
   exit 1
 fi
 
