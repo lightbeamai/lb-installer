8ba9fad46155ba60f80bc808da9ec4e0ee38fffd
root <root@ip-172-31-44-98.us-east-2.compute.internal>
2024-02-13 07:14:11 +0000

    LA-10122 AD based authn for azure mssql

--- sql_user_check_sql_server/database_stats.sh
+++ sql_user_check_sql_server/database_stats.sh
@@ -2,10 +2,12 @@
 
 mode="stats"
 port="1433"
+use_ad_auth=0
+trust_server_cert=0
 
 set -e
 
-while getopts h:u:d:o:m:p: flag
+while getopts h:u:d:o:m:p:a:t: flag
 do
     case "${flag}" in
         h) dbhost=${OPTARG};;
@@ -14,11 +16,13 @@ do
         o) outputfile=${OPTARG};;
         m) mode=${OPTARG};;
         p) port=${OPTARG};;
+	a) use_ad_auth=${OPTARG};;
+	t) trust_server_cert=${OPTARG};;
     esac
 done
 
 
-echo "dbhost: $dbhost username: $username database: $database mode: $mode outputfile: $outputfile port: $port";
+echo "dbhost: $dbhost username: $username database: $database mode: $mode outputfile: $outputfile port: $port AD Auth: $use_ad_auth Trust Server Cert: $trust_server_cert";
 
 # Check if sqlcmd command is available
 if ! command -v sqlcmd &>/dev/null; then
@@ -31,13 +35,26 @@ if [ -z "$dbhost" ] || [ -z "$username" ] || [ -z "$database" ] || [ -z "$output
         exit 1
 fi
 
+auth_flag=""
+password=""
+if [ "$use_ad_auth" -eq 1 ]; then
+    auth_flag="-G"  # Use AD authentication
+    read -p "Password: " password
+    password="-P $password"
+fi
+
+trust_cert_flag=""
+if [ "$trust_server_cert" -eq 1 ]; then
+    trust_cert_flag="-C"  # Trust server certificate
+fi
+
 
 if [ "$mode" == "stats" ]; then
-  sqlcmd -S $dbhost -U $username -i ./database_list_with_size.sql -i ./data_type_distribution.sql -i ./other_stats.sql\
-  -d $database -o $outputfile
+  sqlcmd -S $dbhost -U $username $password -i ./database_list_with_size.sql -i ./data_type_distribution.sql -i ./other_stats.sql\
+  -d $database -o $outputfile $auth_flag $trust_cert_flag
 
 elif [ "$mode" == "full_metadata" ]; then
-  sqlcmd -S $dbhost -U $username -i ./queries.sql -d $database -o $outputfile
+  sqlcmd -S $dbhost -U $username $password -i ./queries.sql -d $database -o $outputfile $auth_flag $trust_cert_flag
 
 else
   echo "Mode should be either stats or full_metadata, found: $mode"
