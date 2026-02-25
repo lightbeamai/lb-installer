fd6e869c03c96d3e0738dd3420edc4a3658b70e2
owais-lb <owais@lightbeam.ai>
2026-02-25 22:40:16 +0530

    review comment

--- RHEL/master.sh
+++ RHEL/master.sh
@@ -8,7 +8,7 @@ fi
 sudo yum update -y
 
 grep -qxF 'export PATH="/usr/local/bin:$PATH"' ~/.bashrc || echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
-export PATH="/usr/local/bin:$PATH"
+source ~/.bashrc
 
 echo "Installing docker"
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/rhel/docker-ce.repo
